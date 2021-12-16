7b93ad5e57b5e85d2239f23f735aa8cfe6f1c537
yuka fujii <batonyf@icloud.com>
2021-12-17 04:12:02 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const { static } = require('express');
+const express = require('express')
+const path = require("path")
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname,"public")))
+
+// app.get('/', function (req, res) {
+//     console.log(req);
+//   res.send('<h1>top</h1>');
+// })
+ 
+app.post('/api/v1/quiz', function (req, res) {
+    const answer = req.body.answer;
+    if(answer === "2") {
+        // res.send("ok!");
+        res.redirect("/correct.html")
+    }else{
+        // res.send("no!");
+        res.redirect("/wrong.html")
+    }
+    res.send(answer);
+  });
+
+app.get('/api/v1/users', function (req, res) {
+    res.send({
+        name:"yuka",
+        age:26
+    });
+  });
+
+app.listen(30001,function(){
+    console.log("I am running!")
+});
